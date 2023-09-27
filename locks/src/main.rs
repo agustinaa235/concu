ee1db33a544cbda612d673b56d224b4616c36c66
agustina235 <asegura@fi.uba.ar>
2023-09-27 20:26:11 -0300

    Fix:dile game

--- locks/src/main.rs
+++ locks/src/main.rs
@@ -16,76 +16,64 @@ use std::collections::HashMap;
 */
 
 
-const ENTIDADES: u32 = 10;
+const ENTIDADES: u32 = 4;
 
 
-#[derive(Debug)]
-struct Game {
-    oro: f64,
-    recursos: HashMap<String,i32>,
-}
+
+const MONTO_INICIAL : f64 = 0.0;
+const RECURSO_INICIAL: i32 = 0;
 
 fn main() {
    
-    let mut recursos = HashMap::new();
-    recursos.insert(String::from("Hamburguesa"), 5);
-    recursos.insert(String::from("Papas fritas"), 10);
-    recursos.insert(String::from("Coca Cola"), 7);
+    let oro = Arc::new(RwLock::new(MONTO_INICIAL));
+    let hamburguesa = Arc::new(RwLock::new(RECURSO_INICIAL));
+    let papas = Arc::new(RwLock::new(RECURSO_INICIAL));
 
-    let game = Arc::new(RwLock::new(Game{oro: 100.0, recursos: recursos}));
+   
     let mut entidades: Vec<JoinHandle<()>> =Vec::new();
-    for id in 1..=ENTIDADES {
-        let game_local = game.clone();
-        let entidad =  thread::spawn(move || play_game(id, game_local));
-        entidades.push(entidad);
-    }
+    
+    let oro_actual = oro.clone();
+    let oro_consumir_hamburguesa = oro.clone();
+    let hamburgesa_actual = hamburguesa.clone();
+    let entidad1 =  thread::spawn(move || producir_oro(1, oro_actual));
+    entidades.push(entidad1);
+
+    let entidad2 =  thread::spawn(move || compro_hamburguesa(2, oro_consumir_hamburguesa, hamburgesa_actual));
+    entidades.push(entidad2);
+    
 
     loop {
         thread::sleep(Duration::from_secs(2));
-        println!("Estado del juego {:?}", game.read());
+        println!("Estado del juego: Oro {:?} | hambuguesas {:?} ", oro.read(), hamburguesa.read());
     }
      
     
     
 }
 
-fn play_game(id:u32, game_local:Arc<RwLock<Game>>){
-
-    // id del rpdoucto y valor del producto
-    let recursos = [ String::from("Hamburguesa"),  String::from("Papas fritas"),  String::from("Coca Cola")];
-    let mut round = 0;
-    while round < 5 {
-
-        if let Ok(mut game) = game_local.write(){
-            if  id == 2 || id == 5 {
-                // extraen oro
-                thread::sleep(Duration::from_millis(1000));
-                let extraigo_oro = game.oro*thread_rng().gen_range(0.1, 0.5);
-                game.oro -= extraigo_oro;
-                println!("[Entidad {}] extraigo oro {}", id, extraigo_oro);
-            } else if id == 1 || id == 3{
-                // producen oro
-                thread::sleep(Duration::from_millis(1000));
-                let agrego_oro = game.oro*thread_rng().gen_range(0.1, 0.5);
-                game.oro += agrego_oro;
-                println!("[Entidad {}] agrego oro {}", id, agrego_oro);
-            } else if id == 8 || id == 7 {
-                // solo produce de a 1 recurso
-                let recurso_a_producir = &recursos[thread_rng().gen_range(1,3)];
-                let cant_a_producir_recurso = game.recursos[recurso_a_producir] + 1;
-                game.recursos.insert(recurso_a_producir.to_string(), cant_a_producir_recurso);
-                println!("[Entidad {}] produzco recurso {} en cantidad {}",id,  &recurso_a_producir, cant_a_producir_recurso);
-            } else {
-                // consumo recurso 
-                let recurso_a_producir = &recursos[thread_rng().gen_range(1,3)];
-                let cant_a_producir_recurso = game.recursos[recurso_a_producir] - 1;
-                
-                game.recursos.insert(recurso_a_producir.to_string(), cant_a_producir_recurso);
-                println!("[Entidad {}] consumo recurso {} en cantidad {}",id,  &recurso_a_producir, cant_a_producir_recurso);
-            }
+fn producir_oro(id:u32, oro_a_producir:Arc<RwLock<f64>>){
+    loop {
+        println!("Thread [{}] que produce oro", id);
+        if let Ok(mut oro) = oro_a_producir.write(){
+            let produzco_oro = 20.0;
+            *oro += produzco_oro;
+            thread::sleep(Duration::from_millis(1000));
         }
-        round +=1;
-       
-        println!("{:?}", game_local.read());
+    }
+}
+
+fn compro_hamburguesa(id:u32, oro_a_consumir:Arc<RwLock<f64>>, hamburguesa_a_producir:Arc<RwLock<i32>>){
+    loop {
+        println!("Thread [{}] que produce hamburgesas", id);
+    
+        if let Ok(mut oro_actual) = oro_a_consumir.write(){
+            if let Ok(mut hamburgesa) = hamburguesa_a_producir.write(){
+                if *oro_actual >= 10.0 {
+                    *oro_actual -= 10.0;
+                    *hamburgesa +=1;
+                }
+            }
+            thread::sleep(Duration::from_millis(1000));
+        }
     }
 }
